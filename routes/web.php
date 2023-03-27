71f03e740a531e66f1ce564a8384ac84ba6c822d
Jesus gutierrez <jesuslgutierrez685@gmail.com>
2023-03-27 14:43:53 -0600

    agregagos rutas web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\CuidadorController;
+use App\Http\Controllers\MedicamentoController;
+use App\Http\Controllers\TratamientoController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +23,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//-----------LOGIN-and Register------------//
+Route::resource('login',RegistroController::class);
+Route::name('validar')->get('validar',[RegistroController::class,'validar']);
+Route::name('logout')->get('logout',[RegistroController::class,'logout']);
+Route::resource('registro',RegistroController::class);
+//------------vistas generales ------------//
+Route::resource('cliente',ClientesController::class);
+Route::resource('cuidador',CuidadorController::class);
+Route::resource('medicamento',MedicamentoController::class);
+Route::resource('tratamiento',TratamientoController::class);
+Route::resource('Drugslide',MaquinaController::class);
