fed8fc9753e28885226da7d884ff264815fcedbc
Jesus gutierrez <jesuslgutierrez685@gmail.com>
2023-03-27 13:23:43 -0600

    agregamos datos alas migracion y esta corriendo bien

---
+++ database/migrations/2023_03_27_185201_cuidadores.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('cuidadores',function (Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->date('fechaN');
+            $table->char('sexo');
+            $table->integer('parentesco');
+            $table->string('lugarTrabajo');
+            $table->integer('matricula');
+            $table->integer('Telefono');
+            $table->string('email')->unique();
+            //$table->integer('imagenP');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('cuidadores');
+    }
+};
